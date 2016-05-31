a99e90f7fd7466ee12c09a850bbe341de0a062bb
will <wferguson@northplains.com>
2016-05-31 10:02:37 +0100

    new stuff

--- src/main/java/com/github/willferguson/nearcache/bootstrap/Bootstrap.java
+++ src/main/java/com/github/willferguson/nearcache/bootstrap/Bootstrap.java
@@ -1,23 +1,15 @@
 package com.github.willferguson.nearcache.bootstrap;
 
-import com.github.willferguson.nearcache.SharedDataKeys;
+import com.github.willferguson.nearcache.verticles.LockTestVerticle;
 import com.github.willferguson.nearcache.verticles.SharedDataReaderVerticle;
 import com.github.willferguson.nearcache.verticles.SharedDataWriterVerticle;
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.config.Config;
-import com.hazelcast.config.NearCacheConfig;
-import com.hazelcast.core.HazelcastInstance;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.impl.VertxInternal;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -28,10 +20,9 @@ public class Bootstrap {
     private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
 
     public static void main(String[] args) throws Exception {
-        CountDownLatch latch = new CountDownLatch(2);
+        CountDownLatch latch = new CountDownLatch(3);
         //Load up the default cluster manager configuration
         ClusterManager manager = new HazelcastClusterManager();
-
         VertxOptions options = new VertxOptions().setClusterManager(manager);
         Vertx.clusteredVertx(options, result -> {
             Vertx vertx = result.result();
@@ -40,12 +31,20 @@ public class Bootstrap {
                     logger.error("Couldn't start DataWriter", res.cause());
 
                 }
+                latch.countDown();
 
             });
             vertx.deployVerticle(SharedDataReaderVerticle.class.getName(), res -> {
                 if(!res.succeeded()) {
                     logger.error("Couldn't start DataReader", res.cause());
                 }
+                latch.countDown();
+            });
+            vertx.deployVerticle(LockTestVerticle.class.getName(), res -> {
+                if(!res.succeeded()) {
+                    logger.error("Couldn't start Lock test", res.cause());
+                }
+                latch.countDown();
             });
         });
 
